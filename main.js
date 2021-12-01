124e80ef5ba2a3aa662f8d864e9651ef1d3700b2
Aritrdey <95201673+Aritrdey@users.noreply.github.com>
2021-12-01 15:45:35 +0530

    g

---
+++ main.js
@@ -0,0 +1,12 @@
+function dropdown(){
+    let b = (document).getElementById('nation');
+
+
+    if(b.style.display === 'none'){
+        b.style.display = 'block';
+    }
+    else{
+        b.style.display = 'none';
+    }
+    
+}
